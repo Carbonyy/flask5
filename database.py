a536c7adfbe32b50060f89f59bc20c196ead2588
218 <forstudy2422@gmail.com>
2024-11-02 10:35:45 +0500

    2. Добавление блоков новостей в главную страницу (2 пара)

--- database.py
+++ database.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_babel import Babel, format_datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['BABEL_DEFAULT_LOCALE'] = 'ru'
+babel = Babel(app)
+
+@app.context_processor
+def inject_conf_vars():
+    return dict(format_datetime=format_datetime)
+
+
+
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'mega_secret_key'
 db = SQLAlchemy(app)
@@ -17,6 +31,16 @@ class User(db.Model):
     age = db.Column(db.Integer, nullable=False)
     speciality = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(200), nullable=False)
+
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=False, nullable=False)
+    content = db.Column(db.String(2000), unique=False, nullable=False)
+    date_of_create = db.Column(db.DateTime, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+news = db.relationship('News', backref='author', lazy=True)
 
 with app.app_context():
     db.create_all()
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from flask import render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash
-from database import app, User, db
+from database import app, User, db, News
 from stats import professions_data
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    news_items = News.query.order_by(News.date_of_create.desc()).all()
+    news_with_authors = []
+    for item in news_items:
+        author = User.query.get(item.author_id)
+        news_with_authors.append((item, author))
     title = request.args.get('title', 'Миссия')
     return render_template('index.html',
                            title=title,
                            mission_name='Поход к роковой горе',
-                           mission_slogan='Сложности встречаются, но мы непременно дойдем до конца!')
+                           mission_slogan='Сложности встречаются, но мы непременно дойдем до конца!',
+                           news=news_with_authors)
 
 
 @app.route('/training/<prof>')
@@ -66,6 +72,13 @@ def home():
         return redirect(url_for('home'))
 
     return render_template('form.html')
+
+@app.route('/news/<int:news_id>')
+def news_detail(news_id):
+    news_item = News.query.get_or_404(news_id)
+    author = User.query.get(news_item.author_id)
+    return render_template('news_item.html', news_item=news_item, author=author)
+
 
 if __name__ == '__main__':
     app.run(debug=False)
